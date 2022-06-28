0c06f9a7e6c1719c55cf8cf252e65b14eb57f28d
Maruthi <maruthirajabn@gmail.com>
2022-06-28 20:37:49 +0530

    This is my first commit

---
+++ CUCUFramework/src/main/java/CUCUFramework/CUCUFramework/App.java
@@ -0,0 +1,13 @@
+package CUCUFramework.CUCUFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
